630e8e5fbd16faeb1fe82d0a3cb6411b6233c632
kevinwiranata <kevin.a.wiranata@gmail.com>
2021-10-15 22:44:06 -0700

    finish lab 3

--- serverTest.py
+++ serverTest.py
@@ -1,7 +1,7 @@
 import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serv.bind(('0.0.0.0', 8000))
+serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
 	conn, addr = serv.accept()
